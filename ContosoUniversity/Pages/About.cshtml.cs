6906b81691ac7b09c7c5394b85ad34122164278f
Nadeem Khan <21814549@bucks.ac.uk>
2020-11-25 16:10:15 +0000

    1st Commit

---
+++ ContosoUniversity/Pages/About.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using ContosoUniversity.Models.SchoolViewModels;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ContosoUniversity.Models;
+
+namespace ContosoUniversity.Pages
+{
+    public class AboutModel : PageModel
+    {
+        private readonly SchoolContext _context;
+
+        public AboutModel(SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public IList<EnrollmentDateGroup> Student { get; set; }
+
+        public async Task OnGetAsync()     // LINQ statement groups student identities by enrollment date,
+                                           // calculates no. of entities in each group,
+                                           // and stores results in a collection of EnrollmentDateGroup view model objects.
+        { 
+            IQueryable<EnrollmentDateGroup> data =
+                from student in _context.Student
+                group student by student.EnrollmentDate into dateGroup
+                select new EnrollmentDateGroup()
+                {
+                    EnrollmentDate = dateGroup.Key,
+                    StudentCount = dateGroup.Count()
+                };
+
+            Student = await data.AsNoTracking().ToListAsync();
+        } 
+    }
+}
